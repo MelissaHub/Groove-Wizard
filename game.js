1f7e98fd3dede6a38f77945e82b130434b288842
MelissaHub <Melissadevelopment2022@gmail.com>
2022-07-18 13:55:09 -0400

    COmpleted buttons

--- game.js
+++ game.js
@@ -4,6 +4,8 @@ let c2 = document.querySelector('.child-2')
 
 let c3 = document.querySelector('.child-3')
 
+let c4 = document.querySelector('.child-6')
+
 let score = document.querySelector('.child-4')
 
 let response = document.querySelector('.child-5')
@@ -20,10 +22,11 @@ c2.innerHTML = 'wind'
 
 c3.innerHTML = 'fire'
 
-score.innerHTML = 'Groove wizard:  <br>User: </br>'
+score.innerHTML = 'Groove:  <br>User: </br>'
 //reassign those selections to a string
 
-
+let userScore = 0
+let computerScore = 0
 
 
 
@@ -41,24 +44,71 @@ c2.addEventListener('click', windbtn)
 
 
 c3.addEventListener('click', firebtn) 
+
+c4.addEventListener('click', reset)
+c4.style.display ='none'
 //whichever is clicked should run through Playround? WHats wrong with them
 
 let  playerSelection =''
 
 function earthbtn (){ 
     playerSelection = 'earth'
- 
+    playRound()
+    refreshStats()
+    winner()
+
 
 }
 function windbtn (){ 
     playerSelection = 'wind'
- 
+    playRound()
+    refreshStats()
+    winner()
 
 }
 function firebtn (){ 
     playerSelection = 'fire'
  
+    playRound()
+    refreshStats()
+    winner()
+    
+}
 
+function winner(){ 
+
+    if (userScore  > computerScore){ 
+        if (userScore === 10){
+         response.innerHTML  = 'You defeated the Groovemaster!'
+         
+         c1.removeEventListener('click', earthbtn) 
+         c2.removeEventListener('click', windbtn) 
+         c3.removeEventListener('click', firebtn) 
+         c4.addEventListener('click', reset)
+         c4.style.display ='block'
+         //remove event listener sig haas to be exaxt same of event listenr when added
+        }
+       }
+       else { 
+        if (computerScore === 10){
+        response.innerHTML  = 'You loose to the Groove!'
+        c1.removeEventListener('click', earthbtn) 
+        c2.removeEventListener('click', windbtn) 
+        c3.removeEventListener('click', firebtn) 
+        c4.addEventListener('click', reset)
+        c1.style.display = 'none'
+        c2.style.display = 'none'
+        c3.style.display = 'none'
+        c4.style.display ='block'
+        }
+       }
+}
+//add button have be click function
+
+function reset(){ 
+
+    document.location.reload(true)
+    //reset
 }
 
 function computerPlay(){ 
@@ -85,25 +135,27 @@ const computerSelection = computerPlay()
         response.innerHTML = 'Your move! earth wind or fire!'
         //want to simply start in box 5, how to get the box to show up???
 
-        let userScore = 0
-
-        let computerScore = 0
+console.log('ran')
+            // if ((userScore >= 0 && computerScore >= 0) || (userScore  < 10 || computerScore < 10)) {
         //both scores start at zero, why not show up
-        switch (computerSelection){ 
+        switch (playerSelection){ 
             case 'earth':
-                switch ( playerSelection){ 
+                switch ( computerSelection){ 
+
                     case 'earth':  
-                    score.innerHTML =`User: ${userScore +0} 'Groove Wizard: ' + ${computerScore -0}`  
+                    // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}`  
                     return response.innerHTML  = "Earth V Earth, NO SCORE!" 
 
-
                     case 'wind':
-                        score.innerHTML =`User: ${userScore +1} 'Groove Wizard: ' + ${computerScore -1}`         
-                    return response.innerHTML  = "Goove chose earth! Groove is hit!" 
+                        computerScore++
+                        // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}`         
+                    return response.innerHTML  = "Goove chose wind! You are hit!" 
 
 
                     case 'fire':
-                        score.innerHTML =`User: ${userScore -1} 'Groove Wizard: ' + ${computerScore +1}`  
+                        userScore++
+
+                        // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}`  
                    return response.innerHTML  = "Groove chose fire! You are hit!" 
 
 
@@ -111,38 +163,45 @@ const computerSelection = computerPlay()
 
                 break
             case 'wind':
-                switch ( playerSelection){ 
+                switch (computerSelection){ 
                     case 'earth': 
-                    score.innerHTML =`User: ${userScore -0} 'Groove Wizard: ' + ${computerScore -1}`  
-                    return response.innerHTML  = "Groove chose wind! They hit themselves!" 
+                    // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}`  
+                    return response.innerHTML  = "Groove chose earth! You hit Yourself!" 
 
                     
                     case 'wind':
-                        score.innerHTML =`User: ${userScore +1} 'Groove Wizard: ' + ${computerScore +0}`     
+                        computerScore++
+                        // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}`     
                      return response.innerHTML  = "Groove chose wind! They empower you!"    
 
 
                     case 'fire':
-                        score.innerHTML =`User: ${userScore -1} 'Groove Wizard: ' + ${computerScore +1}` 
-                    return response.innerHTML  = "Groove chose wind! They extinguish your flame!"
+                        userScore++
+
+                        // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}` 
+                    return response.innerHTML  = "Groove chose fire! They extinguish Their flame!"
 
 
                 }
             break
             case 'fire':
-                switch ( playerSelection){ 
+                switch ( computerSelection){ 
                     case 'earth': 
-                    score.innerHTML =`User: ${userScore -1} 'Groove Wizard: ' + ${computerScore +0}` 
+
+                    // score.innerHTML =`User: ${userScore } Groove Wizard:   ${computerScore}` 
                     return response.innerHTML   = "Groove chose fire! They are not affected" 
 
 
                     case 'wind':
-                    score.innerHTML =`User: ${userScore +0} 'Groove Wizard: ' + ${computerScore -1}`     
+                  userScore++    
+                    // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}`     
                     return response.innerHTML   = "Groove chose fire! Groove gets extinguished" 
 
 
                     case 'fire':
-                        score.innerHTML =`User: ${userScore +1} 'Groove Wizard: ' + ${computerScore +1}`         
+                        userScore++
+                        computerScore++
+                        // score.innerHTML =`User: ${userScore} Groove Wizard:   ${computerScore}`         
                     return response.innerHTML   = "Groove chose fire! You both are empowered!" 
 
 
@@ -150,25 +209,28 @@ const computerSelection = computerPlay()
 
                 break
         }
-
     }
 
+   
+    
         function startGame(){ 
 
-        while(userScore || computerScore < 10){ 
-        playRound()
-        return scoreBoard = 'Grovewizard :' + computerScore + 'User: ' + userScore
+    c1.addEventListener('click', earthbtn) 
+    
+    c2.addEventListener('click', windbtn) 
+
+
+    c3.addEventListener('click', firebtn) 
         }
         //while less that ten, if either is more it responds the if statement
-
-       if (userScore  > computerScore){ 
-        return response = 'You defeated the Groovemaster!'
-        
-       }
-       else { 
-        return response = 'You loose to the Groove!'
-       }
+    
     //need a reset
+    
+
+    startGame()
+
+    function refreshStats(){ 
+      score.innerHTML = 'Groove: ' + computerScore +'<br>' + 'User: ' + userScore 
     }
 
         // if(playerSelection == 'earth' && computerSelection == 'earth'){
